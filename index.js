9f9f6c48f87819d69f5e7d4b8bf47d4df17acce8
Stephanie Sison <sjsison27@icloud.com>
2022-06-13 22:30:22 -0700

    commit tree a8b74e2cf9a695cf31c3c581a7a0d2595c09fd11

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function () => {
+//    document.getElementById('text').textContent = 'This is really cool!' 
+// })
+
+const textChange = document.getElementById('text');
+document.addEventListener('DOMContentLoaded', function() {
+    textChange.textContent = 'This is really cool!';
+})
